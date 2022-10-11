969070a2ea40021a9a760d5c9fcb0f9653a032e6
ankushcode1 <115027801+ankushcode1@users.noreply.github.com>
2022-10-11 14:16:05 +0200

    Create largest_1.js
    without math library

---
+++ largest_1.js
@@ -0,0 +1,21 @@
+//largest among three numbers
+
+//nput
+const num1 = parseFloat(prompt("Enter first number: "));
+const num2 = parseFloat(prompt("Enter second number: "));
+const num3 = parseFloat(prompt("Enter third number: "));
+let largest;
+
+//condition
+if(num1 >= num2 && num1 >= num3) {
+    largest = num1;
+}
+else if (num2 >= num1 && num2 >= num3) {
+    largest = num2;
+}
+else {
+    largest = num3;
+}
+
+//result
+console.log("The largest number is " + largest);
